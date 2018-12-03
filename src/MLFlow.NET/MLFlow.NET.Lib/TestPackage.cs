6bad8e5eeabd077889367e7b24de19134b4f05dd
Jordan Knight <jakkaj@gmail.com>
2018-12-04 10:35:31 +1100

    Created package

---
+++ src/MLFlow.NET/MLFlow.NET.Lib/TestPackage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MLFlow.NET.Lib
+{
+    public class TestPackage
+    {
+        public string HiTehre()
+        {
+            return "Hi there";
+        }
+    }
+}
