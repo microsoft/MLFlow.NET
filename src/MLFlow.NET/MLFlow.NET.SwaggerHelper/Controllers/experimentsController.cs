d11d6d40d84ef129bf84b209dfeafe401766829f
Jordan Knight <jakkaj@gmail.com>
2018-12-04 16:40:37 +1100

    Added a site to help us generate swagger

---
+++ src/MLFlow.NET/MLFlow.NET.SwaggerHelper/Controllers/experimentsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MLFlow.NET.SwaggerHelper.Controllers
+{
+    [Route("2.0/preview/mlflow/experiments")]
+    public class experimentsController : Controller
+    {
+        /// <summary>
+        /// Create Experiment
+        /// </summary>
+        /// <remarks>
+        /// Create an experiment with a name. Returns the ID of the newly created experiment.
+        /// Validates that another experiment with the same name does not already exist and fails if another experiment with the same name already exists.
+        /// Throws RESOURCE_ALREADY_EXISTS if a experiment with the given name exists.
+        /// </remarks>
+        /// <param name="name">Experiment name. This field is required.</param>
+        /// <param name="artifact_location">Location where all artifacts for the experiment are stored. If not provided, the remote server will select an appropriate default.</param>
+        /// <returns></returns>
+        [Route("create")]
+        [HttpPost]
+        public IActionResult create([Required]string name, string artifact_location = null)
+        {
+            return null;
+        }
+    }
+}
